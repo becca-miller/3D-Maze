494cc4d4749f46bc70e15cc7093fcbbc6f1b1229
Becca <bemiller627@gmail.com>
2017-11-15 21:21:19 -0500

    Added textures to maze and implemented timer

---
+++ Assets/TimerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    static float timer = 0.0f;
+    public Text text_box;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        text_box.text = timer.ToString("0.00");
+    }
+}
